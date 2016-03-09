cd9607bc867996f4038a12883e76e09aeed56e4f
Jacek Szubert <jacek.szubert@gmail.com>
2016-03-09 15:52:52 +0100

    installing git moved to cloud-formation ofc

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -7,7 +7,7 @@ export quake_port
 export COMPOSE_API_VERSION=1.18
 
 apt-get update
-apt-get -y install docker.io git python-pip aria2
+apt-get -y install docker.io python-pip aria2
 pip install docker-compose
 aria2c -x 16 -s 16 http://game.pioneernet.ru/dl/q3/files/pk3/pak0.pk3 --dir="${scriptdir}"
 
